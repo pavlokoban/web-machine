0958bde2147ce14a4d0b14ee89ca060e64fd29e0
pavlokoban <109820459+pavlokoban@users.noreply.github.com>
2024-07-09 14:35:41 +0300

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+
+export default function Layout(){
+return (
+    <div>
+        <head>
+            <title>test title</title>
+            <meta name="description" content="test description" />
+            <link rel="icon" href="/favicon.ico" />
+        </head>
+            <header>Header</header>
+            <main>Main</main>
+            <footer>Footer</footer>
+    </div>
+);
+};
