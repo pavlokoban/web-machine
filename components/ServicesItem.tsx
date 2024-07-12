0893a41148c4d906369b24a07d998279cca8faed
Web Machine Rocks <109820459+pavlokoban@users.noreply.github.com>
2024-07-12 20:35:09 +0300

    Update ServicesItem.tsx

--- components/ServicesItem.tsx
+++ components/ServicesItem.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import Link from 'next/link';
-import dataArticles from "../utils/dataArticles";
-import Image from 'next/image';
 import { Article } from './types'; // Импорт интерфейса
 
 interface ServicesItemProps {
@@ -9,37 +7,23 @@ interface ServicesItemProps {
 }
 
 export default function ServicesItem({ article }: ServicesItemProps) {
-    return (
-        <div className={'max-w-sm rounded-md overflow-hiddens shadow-lg'}>
-        <Link href={`/${article.slug}`}>
-        <img src={article.image}
-                alt={article.title}
-                className={'rounded shadow w-full'}
+  return (
+    <div className={'max-w-sm rounded-md overflow-hidden shadow-lg'}>
+      <Link href={`/${article.slug}`}>
+        <img 
+          src={article.image}
+          alt={article.title}
+          className={'rounded shadow w-full'}
         />
-                {article.title + ' ' + article.description}
-        </Link>
-            <div className={'flex flex-col justify-center items-center p-5'}>
-                <h2 className={'font-bold text-center text-xl mb-2'}>{article.title}</h2>
-                <h3 className={'text-center mb-2'}>{article.description}</h3>
-                <button type="button" className={'bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md'}>More</button>
-            </div>
+        <div>
+          {article.title + ' ' + article.description}
         </div>
-)
-}
-    return (
-        <div className={'max-w-sm rounded-md overflow-hiddens shadow-lg'}>
-        <Link href={`/${article.slug}`}>
-        <img src={article.image}
-                alt={article.title}
-                className={'rounded shadow w-full'}
-        />
-                {article.title + ' ' + article.description}
-        </Link>
-            <div className={'flex flex-col justify-center items-center p-5'}>
-                <h2 className={'font-bold text-center text-xl mb-2'}>{article.title}</h2>
-                <h3 className={'text-center mb-2'}>{article.description}</h3>
-                <button type="button" className={'bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md'}>More</button>
-            </div>
-        </div>
-)
+      </Link>
+      <div className={'flex flex-col justify-center items-center p-5'}>
+        <h2 className={'font-bold text-center text-xl mb-2'}>{article.title}</h2>
+        <h3 className={'text-center mb-2'}>{article.description}</h3>
+        <button type="button" className={'bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md'}>More</button>
+      </div>
+    </div>
+  );
 }
