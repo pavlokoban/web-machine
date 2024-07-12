4b0751b0da94d1e9c7f5e053479ab5e999ff8101
Web Machine Rocks <109820459+pavlokoban@users.noreply.github.com>
2024-07-12 20:04:55 +0300

    Update LayoutMain.tsx

--- components/LayoutMain.tsx
+++ components/LayoutMain.tsx
@@ -3,37 +3,39 @@ import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import MainComponent from './MainComponent';
+import { ReactNode } from 'react';
 
-export default function LayoutMain({children}){
-return (
+interface LayoutMainProps {
+  children: ReactNode;
+}
+
+const LayoutMain: React.FC<LayoutMainProps> = ({ children }) => {
+  return (
     <>
-        <body>
-        <div className={'flex min-h-screen flex-col justify-between'}>
-            <header>
-                <nav className={'flex h-12 items-center p-4 justify-between shadow-md'}>
-                        <Link className={'text-lg font-bold'} href="/">
-                        <img src='/next.svg' width={100} height={100}/>
-                        </Link>
-                        
-                        <div className={'p-1'}>
-                        <Link className={'text-lg p-2'} href="/">
-                            Web Development
-                        </Link>
-                        <Link className={'text-lg p-2'} href="/">
-                            SEO
-                        </Link>    
-                        <Link className={'text-lg p-2'} href="/">
-                           Advertisiment
-                        </Link>  
-                        </div>
-                </nav>
-            </header>
-            <main>{ children }</main>
-
-            <footer className={'flex h-10 item-center shadow-inner justify-center'}>Footer</footer>
-        </div>
-        </body>
-
+      <div className={'flex min-h-screen flex-col justify-between'}>
+        <header>
+          <nav className={'flex h-12 items-center p-4 justify-between shadow-md'}>
+            <Link className={'text-lg font-bold'} href="/">
+              <img src='/next.svg' width={100} height={100} />
+            </Link>
+            <div className={'p-1'}>
+              <Link className={'text-lg p-2'} href="/">
+                Web Development
+              </Link>
+              <Link className={'text-lg p-2'} href="/">
+                SEO
+              </Link>
+              <Link className={'text-lg p-2'} href="/">
+                Advertisiment
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer className={'flex h-10 item-center shadow-inner justify-center'}>Footer</footer>
+      </div>
     </>
-);
+  );
 };
+
+export default LayoutMain;
